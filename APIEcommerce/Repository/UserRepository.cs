0c5f3756974e79b8939a9b164a5aec0a051eb20b
PacoChinchay <pacochinchay350@gmail.com>
2025-12-05 01:04:55 -0500

    80 81. Desarrollo del método login, configuración del jwt

--- APIEcommerce/Repository/UserRepository.cs
+++ APIEcommerce/Repository/UserRepository.cs
@@ -2,15 +2,22 @@
 using APIEcommerce.Models;
 using APIEcommerce.Models.Dtos;
 using APIEcommerce.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace APIEcommerce.Repository
 {
     public class UserRepository : IUserRepository
     {
         private readonly ApplicationDbContext _db;
-        public UserRepository(ApplicationDbContext db)
+        private string? secretKey;
+        public UserRepository(ApplicationDbContext db, IConfiguration configuration)
         {
             _db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:SecretKey");
         }
         public User? GetUser(int id)
         {
@@ -27,9 +34,74 @@ namespace APIEcommerce.Repository
             return !_db.Users.Any(u => u.Username.ToLower().Trim() == username.ToLower().Trim());
         }
 
-        public Task<UserLoginResponseDto> Login(UserLoginDto userLoginDto)
+        public async Task<UserLoginResponseDto> Login(UserLoginDto userLoginDto)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(userLoginDto.Username))
+            {
+                return new UserLoginResponseDto()
+                {
+                    Token = "",
+                    User = null,
+                    Message = "El username es requerido"
+                };
+            }
+
+            var user = await _db.Users.FirstOrDefaultAsync<User>(u => u.Username.ToLower().Trim() == userLoginDto.Username.ToLower().Trim());
+            if (user == null)
+            {
+                return new UserLoginResponseDto()
+                {
+                    Token = "",
+                    User = null,
+                    Message = "usuario no encontrado"
+                };
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(userLoginDto.Password, user.Password))
+            {
+                return new UserLoginResponseDto()
+                {
+                    Token = "",
+                    User = null,
+                    Message = "contraseña incorrecta"
+                };
+            }
+
+            // Generar el token JWT
+
+            var handlerToken = new JwtSecurityTokenHandler();
+            if (string.IsNullOrWhiteSpace(secretKey))
+            {
+                throw new InvalidOperationException("SecretKey no está configurada");
+            }
+
+            var key = Encoding.UTF8.GetBytes(secretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("id", user.Id.ToString()),
+                    new Claim("username", user.Username),
+                    new Claim(ClaimTypes.Role, user.Role ?? string.Empty),
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = handlerToken.CreateToken(tokenDescriptor);
+            return new UserLoginResponseDto()
+            {
+                Token = handlerToken.WriteToken(token),
+                User = new UserRegisterDto()
+                {
+                    Username = user.Username,
+                    Name = user.Name,
+                    Role = user.Role,
+                    Password = user.Password ?? ""
+                },
+                Message = "Usuario Logueado Correctamente"
+            };
+
         }
 
         public async Task<User> Register(CreateUserDto createUserDto)
